a39823b8e1d1136f21df9c680399adb07cd0f7ce
cyclerunner1 <meertrompet@gmail.com>
2021-12-08 22:23:52 +0100

    override the statefunction

--- bathroomfan/errorfanstate.h
+++ bathroomfan/errorfanstate.h
@@ -9,13 +9,6 @@ public:
     virtual void Handle1() override;
     virtual void Handle2() override;
 
-    //FUNCTIE MAKEN IN CPP FILE
-//    virtual void E_START() override;
-//    virtual void E_RUN() override;
-//    virtual void E_STOP() override;
-//    virtual void E_ERROR() override;
-//    virtual void E_FIXED() override;
-
     virtual ~ErrorFanState();
 };
 
